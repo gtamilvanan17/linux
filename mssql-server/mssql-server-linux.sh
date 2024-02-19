0dfb9db5b9b2560cf5fd20a4eea7d352811f7a4c
Tamilvanan Gowran <121214873+gtamilvanan17@users.noreply.github.com>
2024-02-19 18:04:39 +0530

    Create mssql-server-linux.sh

---
+++ mssql-server/mssql-server-linux.sh
@@ -0,0 +1,19 @@
+curl -fsSL https://packages.microsoft.com/keys/microsoft.asc | sudo gpg --dearmor -o /usr/share/keyrings/microsoft-prod.gpg
+curl https://packages.microsoft.com/keys/microsoft.asc | sudo tee /etc/apt/trusted.gpg.d/microsoft.asc
+curl -fsSL https://packages.microsoft.com/config/ubuntu/22.04/mssql-server-2022.list | sudo tee /etc/apt/sources.list.d/mssql-server-2022.list
+sudo apt update && sudo apt install -y mssql-server
+sudo /opt/mssql/bin/mssql-conf setup
+systemctl status mssql-server --no-pager
+curl https://packages.microsoft.com/keys/microsoft.asc | sudo tee /etc/apt/trusted.gpg.d/microsoft.asc
+curl https://packages.microsoft.com/config/ubuntu/22.04/prod.list | sudo tee /etc/apt/sources.list.d/mssql-release.list
+sudo apt update && sudo apt install -y mssql-tools18 unixodbc-dev
+echo 'export PATH="$PATH:/opt/mssql-tools18/bin"' >> ~/.bash_profile
+echo 'export PATH="$PATH:/opt/mssql-tools18/bin"' >> ~/.bashrc
+source ~/.bashrc
+
+
+sqlcmd -S localhost -U sa -P '<YourPassword>'
+CREATE DATABASE TestDB;
+SELECT Name from sys.databases;
+GO
+QUIT
